f3c7d66f6eeb2bc17c3e83b9960b15d68d6963ac
ewrika <egorkaomsk_2003@mail.ru>
2025-03-15 14:51:32 +0600

    Initial Commit

---
+++ StrangeEqualizers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrangeEqualizers
+//
+//  Created by Георгий Борисов on 15.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
